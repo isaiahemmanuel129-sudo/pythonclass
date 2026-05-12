f9fd507d09b5cadae6b6b77aaa829bfaf5eb8a92
Emmie Yung <isaiahemmanuel129@gmail.com>
2026-05-12 14:10:59 +0100

    ggh

---
+++ class.py
@@ -0,0 +1,49 @@
+minePosition = "17km"
+mineBroPosition = "20km"
+
+sliceMinePosition = int(minePosition[0:2])
+print(type(sliceMinePosition))
+
+slicemineBroPosition = int(mineBroPosition [0:2])
+print(type (slicemineBroPosition))
+
+
+# for i in range(1,19, 4):
+#     print(i)
+
+numb1 = 4
+numb2 =5
+
+for i in range(numb1):
+    for j in range(numb2,0,-1):
+        # print(i,j)
+        if i == j:
+            print("perfect")
+            # break
+            # continue
+        else:
+            print(f"{i}km ---🚗🚗🚗🚗> {j}km")
+
+
+# nul = 5
+# for p in range()
+        
+# for i in range(1,11):
+
+
+# for j in range(0, 100, 2):
+#     print(j)
+
+
+# for i in range(0,100,-1):
+#     print(i)
+
+
+# for i in range(100,0,-1):
+#     print(i)
+
+
+# for i in range(numb1,0, -1):
+#     print(i)
+
+
